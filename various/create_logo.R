72ae3a0030f64602b6feeddecdcfc68af1f06e92
Adrian Maldet - B <maldet@xlwt0006.statistik.local>
2019-09-06 11:16:08 +0200

    Update logo

--- various/create_logo.R
+++ various/create_logo.R
@@ -2,13 +2,14 @@
 # install.packages("hexSticker")
 hexSticker::sticker(
     "man/figures/labelmachine.png",
-    package = "labelmachine", 
+    package = c("label       ","     machine"), 
     p_size = 16,
-    p_y = 0.8,
+    p_y = 0.7,
+    p_x = c(0.63, 1.15),
     s_x=1,
-    s_y=1.3,
+    s_y=1.15,
     h_fill = "#17d2e1",
-    p_color = "#ffffff",
+    p_color = c("#a635d0", "#ffffff"),
     h_color = "#a635d0",
     s_width=0.7,
     s_height=0.7,
