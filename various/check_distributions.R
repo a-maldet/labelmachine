4c0b3c31321e2495849866ba6bad0c8797230194
Adrian Maldet - B <maldet@xlwt0006.statistik.local>
2019-09-20 11:51:44 +0200

    Update cran-comments.md

--- various/check_distributions.R
+++ various/check_distributions.R
@@ -1,4 +1,5 @@
 # check all important distribuations for CRAN release
+devtools::build()
 chk <- rhub::check_for_cran(path = "../labelmachine_1.0.0.tar.gz")
 chk$cran_summary()
 sink("various/cran-comments.md")
